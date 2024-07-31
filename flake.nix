9204ce2db63dbcee0c998a7b2746eaefb23b1d38
take-cheeze <takechi101010@gmail.com>
2024-08-01 01:17:29 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          packages = rec {
+            build = pkgs.stdenv.mkDerivation {
+              name = "builder";
+              nativeBuildInputs = with pkgs; [
+                ninja
+                cmake
+                ruby
+                ccache
+              ];
+              buildInputs = with pkgs; [
+                SDL2
+              ];
+              CMAKE_CXX_COMPILER_LAUNCHER = "ccache";
+            };
+            default = build;
+          };
+        }
+      );
+}
