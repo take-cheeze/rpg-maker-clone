10736d0f561c8852c6ddf1451c218b6454830554
take-cheeze <takechi101010@gmail.com>
2024-11-24 22:22:23 +0900

    Fix build

--- mruby-rgss/src/lib.cxx
+++ mruby-rgss/src/lib.cxx
@@ -1,10 +1,8 @@
 #include <mruby.h>
 
-
-
 extern "C" void mrb_mruby_rgss_gem_init(mrb_state* M) {
-    RClass* m = mrb_define_module(M, "RGSS")
-    RClass* bmp = mrb_define_class_under(M, bmp, "Bitmap", M->object_class);
+    RClass* m = mrb_define_module(M, "RGSS");
+    RClass* bmp = mrb_define_class_under(M, m, "Bitmap", M->object_class);
 }
 
 extern "C" void mrb_mruby_rgss_gem_final(mrb_state* mrb) {
