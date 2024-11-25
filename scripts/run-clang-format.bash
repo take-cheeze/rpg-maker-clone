18bbda5f2dff07287fd672e20b1b7d8947f3c655
take-cheeze <takechi101010@gmail.com>
2024-11-25 15:16:10 +0900

    Use pre-commit to lint

--- scripts/run-clang-format.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux -o pipefail
-
-cd $(git rev-parse --show-toplevel)
-
-git ls-files '*.cc' '*.cxx' | xargs -P$(nproc) clang-format -i
-
-git diff --exit-code
--- scripts/run-cmake-format.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux -o pipefail
-
-cd $(git rev-parse --show-toplevel)
-
-git ls-files '*.cmake' 'CMakeLists.txt' | xargs -P$(nproc) cmake-format -i
-
-git diff --exit-code
