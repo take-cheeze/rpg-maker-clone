bc77eeeb9853dc05eec406e292a386d128143ab5
take-cheeze <takechi101010@gmail.com>
2024-12-07 23:02:42 +0900

    Install fonts

--- scripts/rtp_install.bash
+++ scripts/rtp_install.bash
@@ -31,8 +31,6 @@ cp setup.iss "${WINEPREFIX}/drive_c/setup.iss"
 
 wine ./RPG2000RTP.exe /s /a /s /sms /f1C:\\setup.iss
 
-find "$WINEPREFIX/drive_c"
-
 ls "$WINEPREFIX/drive_c/Program Files (x86)/ASCII"
 
 kill %1
