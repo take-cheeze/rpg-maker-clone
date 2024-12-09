c299537d9a355a553553a1abc939b2045c1731d7
take-cheeze <takechi101010@gmail.com>
2024-12-09 15:45:34 +0900

    Use software sdl renderer for WSL

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -836,9 +836,10 @@
 #if LV_USE_SDL
     #define LV_SDL_INCLUDE_PATH    <SDL2/SDL.h>
     #define LV_SDL_RENDER_MODE     LV_DISPLAY_RENDER_MODE_DIRECT   /*LV_DISPLAY_RENDER_MODE_DIRECT is recommended for best performance*/
-    #define LV_SDL_BUF_COUNT       1    /*1 or 2*/
+    #define LV_SDL_BUF_COUNT       2    /*1 or 2*/
     #define LV_SDL_FULLSCREEN      0    /*1: Make the window full screen by default*/
     #define LV_SDL_DIRECT_EXIT     1    /*1: Exit the application when all SDL windows are closed*/
+    #define LV_SDL_ACCELERATED     0    /**< 1: Use hardware acceleration*/
 #endif
 
 /*Use X11 to open window on Linux desktop and handle mouse and keyboard*/
