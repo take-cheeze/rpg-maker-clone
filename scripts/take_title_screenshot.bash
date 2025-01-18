d2942f694cc9c2d785dceb0865ccbaec0a31f9cf
take-cheeze <takechi101010@gmail.com>
2025-01-19 01:46:27 +0900

    Take wine screenshot on CI

---
+++ scripts/take_title_screenshot.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+cd $(dirname $0)/..
+
+mkdir -p ss
+
+cat <<EOS > ./scripts/docker_script.bash
+set -eux -o pipefail
+
+# wine winecfg /v
+# wineserver -w
+winetricks -q fakejapanese_ipamona
+
+wine /proj/data/Nepheshel206beta/Nepheshel206Rbeta/RPG_RT.exe &
+sleep 8
+xwd -root -silent | convert xwd:- png:/proj/ss/title.png
+xdotool keydown Up
+sleep 0.1
+xdotool keyup Up
+xwd -root -silent | convert xwd:- png:/proj/ss/title2.png
+xdotool keydown Return
+sleep 0.1
+xdotool keyup Return
+EOS
+
+docker-wine \
+    --local=takecheeze/docker-wine \
+    --volume=.:/proj \
+    --xvfb=:95,0,320x240x24 \
+    bash /proj/scripts/docker_script.bash
